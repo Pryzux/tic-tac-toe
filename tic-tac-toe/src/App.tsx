411b519e6b85437482e03f70da1013cef53e065f
Jared <jaredviolo@hotmail.com>
2025-09-22 12:01:21 -0400

    added env + initial table for the tic-tac-toe-board

---
+++ tic-tac-toe/src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import ticTacToeLogo from './assets/tic-tac-toe-icon.svg'
+import './App.css'
+import Board from './tic-tac-toe-board'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={ticTacToeLogo} className="logo" alt="Tic Tac Toe logo" />
+        </a>
+      
+      </div>
+
+      <h1>Tic Tac Toe</h1>
+
+      <div className="card">
+        
+        <Board />
+
+      </div>
+      
+      <p className="read-the-docs">
+        Click a square to play
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ tic-tac-toe/src/tic-tac-toe-board.tsx
@@ -0,0 +1,39 @@
+import './App.css'
+
+function Board() {
+
+  return (
+
+
+    // styling not finished - learning tailwind css after
+    <table className="board-styling">
+
+    {/* Row 1 */}
+    <tr>  
+        <td>x</td>
+        <td>x</td> 
+        <td>x</td>  
+    </tr>
+    {/* Row 2 */}
+    <tr> 
+        <td>x</td> 
+        <td>x</td> 
+        <td>x</td> 
+    </tr>
+    {/* Row 3 */}
+    <tr> 
+        <td>x</td>
+        <td>x</td> 
+        <td>x</td> 
+    </tr>
+
+    </table>
+
+
+
+      
+
+  )
+}
+
+export default Board
