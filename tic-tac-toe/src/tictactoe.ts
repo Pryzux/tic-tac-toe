26c59d6a6edaca606280a59dd2934b950af5e9cd
Jared <jaredviolo@hotmail.com>
2025-09-22 22:30:26 -0400

    feedback, fixed draw/win logic, initial phase complete

--- tic-tac-toe/src/tictactoe.ts
+++ tic-tac-toe/src/tictactoe.ts
@@ -1,13 +1,14 @@
-// Data Structures
-
+//Structure
 export type GameState = {
   board: ('X' | 'O' | null)[][]
   winner: 'X' | 'O' | 'Draw' | null
   player: 'X' | 'O' | null
+  feedback: string
+  gameStatus: string
 
 }
 
-
+//Default
 export const initialGameState: GameState = {
   board: [
     [null, null, null],
@@ -15,20 +16,18 @@ export const initialGameState: GameState = {
     [null, null, null]
   ],
   winner: null,
-  player: 'X'
+  player: 'X',
+  feedback: 'Select a tile to play',
+  gameStatus: 'Not Started'
   }
 
 
-// Helpers
+//---Helpers---
 
-function check_winner(currentGame: GameState): Boolean {
+//need to fix
+function winner(currentGame: GameState): Boolean {
 
   const board = currentGame.board;
-
-  if (is_draw(currentGame)) {
-    console.log("Game is a draw")
-    return false
-  }
 
   if (board[0][0] !== null && board[0][0] === board[0][1] && board[0][1] === board[0][2]) {
     return true;
@@ -57,10 +56,25 @@ function check_winner(currentGame: GameState): Boolean {
   return false
 
 }
+//need to fix
+function draw(currentGame: GameState): Boolean {
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; i<3; i++) {
+      if (currentGame.board[i][j] == null){
+        return false
+      }
+    }  
+}
+return true
+}
 
-function is_valid_move(currentGame: GameState,row:number,col:number): Boolean {
 
-  
+
+function valid_move(currentGame: GameState,row:number,col:number): Boolean {
+
+  if (currentGame.gameStatus == 'Ended') {
+    return false
+  }
 
   if (currentGame.board[row][col] != null) {
     console.log("Invalid Move")
@@ -71,43 +85,56 @@ function is_valid_move(currentGame: GameState,row:number,col:number): Boolean {
 
 }
 
-function is_draw(currentGame: GameState): Boolean {
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; i<3; i++) {
-      if (currentGame.board[i][j] == null){
-        return false
+//---Exposed---
+
+export function makeMove(currentGame: GameState, row: number, col: number): GameState {
+
+  const newGameState = structuredClone(currentGame);
+
+  if (valid_move(newGameState, row, col)) {
+   
+    newGameState.board[row][col] = currentGame.player;
+
+    
+    if (winner(newGameState)) {
+      newGameState.feedback = newGameState.player + ' has won!';
+      newGameState.gameStatus = 'Ended'
+    } else if (draw(newGameState)) {
+      newGameState.feedback = 'The game is a draw';
+      newGameState.gameStatus = 'Ended'
+    } else {
+      change_player(newGameState);
+    }
+  }
+
+  return newGameState;
+}
+
+
+
+
+export function reset_game():GameState {
+  return initialGameState
+}
+
+export function change_player(GameState:GameState):GameState {
+
+  if (GameState.player === 'X') {
+      GameState.player = 'O'
+      if (GameState.gameStatus === 'Ended') {
+        return GameState
       }
-    }  
+      GameState.feedback = "It is 0's Turn, Please Select a Box"
+      return GameState
+    }
+    else if (GameState.player === 'O') {
+      GameState.player = 'X'
+      if (GameState.gameStatus === 'Ended') {
+        return GameState
+      }
+      GameState.feedback = "It is X's Turn, Please Select a Box"
+      return GameState
+    }
+    //idk how to write this without needing a pointless return
+    return GameState
 }
-
-return true
-}
-
-
-export function makeMove(currentGame: GameState, row:number, col:number): GameState {
-
-  const newGameState = structuredClone(currentGame)
-
-  if (newGameState.player === 'X') {
-    newGameState.player = 'O'
-  }
-  else if (newGameState.player === 'O') {
-    newGameState.player = 'X'
-  }
-
-
-  if (is_valid_move(currentGame,row,col)) {
-    newGameState.board[row][col] = currentGame.player
-
-  }
-  
-  if (check_winner(newGameState)) {
-      newGameState.winner = newGameState.player
-      console.log("Winner is: " + newGameState.winner)
-      return initialGameState
-  }
-  
-  return newGameState
-
-}
-
